835de5412e3a7f8cb283dd719ad409f0ceddf12b
Oleh Didyk <oleggsa@gmail.com>
2022-02-28 12:05:37 +0200

    Header section

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
-
+  <div>
+    <Header/>
+  </div>
 </template>
 
 <script>
+import Header from "@/components/Header";
 
+export default {
+  components: {Header}
+}
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap');
 
+* {
+  box-sizing: border-box;
+  font-family: 'Raleway', sans-serif;
+}
+
+body {
+  background: #455db8;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  max-width: 940px;
+  margin: 0 auto;
+}
 </style>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="header">
+    <div class="container">
+      <div class="header__text">
+        <div class="header__text title">
+          Customer relations management application
+        </div>
+        <div class="header__text subtitle">
+          Love to travel CRM
+        </div>
+        <div class="header__text desqr">
+          The goal of the application is to have a CRM fully customized for internal purposes.<br>
+          Application has a streamline process which optimizes the process of working with tourists,
+          helps to track statistics, automates most of the processes and automatically adds accounting
+          records and exports accounting reports.
+        </div>
+        <div class="header__text-img">
+          <img src="../../../images/top-screenshot.png" alt="screenshot">
+        </div>
+        <div class="header__image"><img src="../../../images/top-screen.png" alt="screen"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+$color: red;
+
+.header {
+  overflow-x: hidden;
+  margin-top: 58px;
+  height: 940px;
+  &__text {
+    width: 500px;
+    display: inline-block;
+    position: relative;
+    & .title {
+      color: white;
+      font-weight: bold;
+      font-size: 70px;
+    }
+    & .subtitle {
+      color: #3B9DE8;
+      font-weight: bold;
+      font-size: 70px;
+      margin-top: 10px;
+    }
+    & .desqr {
+      color: white;
+      font-weight: 600;
+      font-size: 18px;
+      margin-top: 20px;
+    }
+    &-img {
+      margin-top: 18px;
+    }
+  }
+  &__image {
+    display: inline-block;
+    position: absolute;
+    top: 134px;
+    left: 584px;
+  }
+}
+</style>
